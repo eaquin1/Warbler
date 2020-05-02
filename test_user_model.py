42a3417a4dbba11fd888d4dc934365738992940a
Emily Aquin <eaquin1@gmail.com>
2020-05-02 12:49:42 -0500

    added user model testing setup and teardown

--- test_user_model.py
+++ test_user_model.py
@@ -34,12 +34,34 @@ class UserModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        u1 = User.signup("test1", "tester1@gmail.com", "password", None)
+        uid1 = 1111
+        u1.id = uid1
+
+        u2 = User.signup("test2", "tester2@gmail.com", "password", None)
+        uid2 = 2222
+        u2.id = uid2
+
+        db.session.commit()
+
+        u1 = User.query.get(uid1)
+        u2 = User.query.get(uid2)
+        
+        self.u1 = u1
+        self.uid1 = uid1
+
+        self.u2 = u2
+        self.uid2 = uid2
 
         self.client = app.test_client()
+
+    def tearDown(self):
+        response = super().tearDown()
+        db.session.rollback()
+        return response
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -56,3 +78,30 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_repr(self):
+        """Does the repr method work as expected?"""
+        user = self.u1
+
+        representation = User.__repr__(user)
+        string = "<User #1111: test1, tester1@gmail.com>"
+
+        self.assertEqual(representation, string)
+
+    #####
+    #
+    # Following Tests
+    #
+    #####
+
+    def test_user_follows(self):
+        """Is User 1 following User 2 detected?"""
+        u1 = self.u1
+        u2 = self.u2
+
+        u1_following = Follows(user_being_followed_id = u2.id, user_following_id=u1.id)
+        db.session.add(u1_following)
+        db.session.commit()
+
+        self.assertTrue(u1.is_following(u2))
+        
